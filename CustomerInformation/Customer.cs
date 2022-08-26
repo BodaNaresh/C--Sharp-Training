e4b19fb679b521edb1afb31c60245239fc23c26f
BodaNaresh <100840377+BodaNaresh@users.noreply.github.com>
2022-08-26 04:37:41 -0700

    commit of day18th Task-1

---
+++ CustomerInformation/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace EcommerceonOOPs
+{
+    public class Customer:MainDetails,ICustomer
+    {
+        public string Address;
+        public double Mobile;
+        public double Pincode;
+
+       public double getcustomersalaryDetails(double salary,double pf,double hra)
+       {
+            double totalsalary = salary * pf * hra / 100;
+            return totalsalary;
+
+       }
+
+        public void DisplayData()
+        {
+            Console.WriteLine("customer details are"+" "+"id:"+Id+" "+"Name:"+Name+" "+"Address:"+Address+" "+"Mobile:"+Mobile+" "+"Pincode"+Pincode);
+        }
+
+    }
+
+
+}
---
+++ productsInformation/IProducts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcommerceonOOPs
+{
+    public interface IProducts
+    {
+        double GetProductDetais(double manucost,double transportcost,double Gst);
+        void displayProddata();
+
+    }
+}
