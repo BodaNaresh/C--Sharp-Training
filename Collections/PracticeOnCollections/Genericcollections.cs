215bcd3bb316475ec6a4930dbc471310fdd45237
BodaNaresh <100840377+BodaNaresh@users.noreply.github.com>
2022-09-06 06:01:23 -0700

    commit of day25 Task-1

---
+++ Collections/PracticeOnCollections/Genericcollections.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeOnCollections
+{
+    public class Genericcollections
+    {
+        public void generics()
+        {
+            List<int> lst = new List<int>();
+            lst.Add(100);
+            lst.Add(200);
+            lst.Add(300);
+            lst.Add(400);
+            lst.Remove(100);
+            foreach (int i in lst)
+            {
+               Console.WriteLine(i);
+            }
+
+            Dictionary<int, string> dct = new Dictionary<int, string>();
+            dct.Add(1, "cs.net");
+            dct.Add(2, "vb.net");
+            dct.Add(3, "vb.net");
+            dct.Add(4, "vb.net");
+            foreach (KeyValuePair<int, string> kvp in dct)
+            {
+                Console.WriteLine(kvp.Key + " " + kvp.Value);
+                Console.WriteLine();
+            }
+
+            SortedList<string, int> sl = new SortedList<string, int>();
+            sl.Add("google",1);
+            sl.Add("microsoft",2);
+            sl.Add("deloitte", 3);
+            sl.Remove("deloitte");
+
+            foreach (KeyValuePair<string, int> kvp in sl)
+            {
+                Console.WriteLine(kvp.Key + " " + kvp.Value);
+                Console.WriteLine();
+            }
+
+            Stack<string> stk = new Stack<string>();
+            stk.Push("cs.net");
+            stk.Push("vb.net");
+            stk.Push("asp.net");
+            stk.Push("sqlserver");
+            stk.Pop();
+
+            foreach (string s in stk)
+            {
+               Console.WriteLine(s);
+            }
+
+            Queue<string> q = new Queue<string>();
+
+            q.Enqueue("cs.net");
+            q.Enqueue("vb.net");
+            q.Enqueue("asp.net");
+            q.Enqueue("sqlserver");
+            q.Dequeue();
+
+            foreach (string s in q)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Collections/PracticeOnCollections/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+
+namespace PracticeOnCollections
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array List
+            Console.WriteLine("--- ArrayList --------");
+            ArrayList al = new ArrayList();
+            string str = "Naresh";
+            int x = 7;
+            DateTime d = DateTime.Parse("8-oct-1985");
+            al.Add(str);
+            al.Add(x);
+            al.Add(d);
+
+            foreach (object o in al)
+            {
+                Console.WriteLine(o);
+            }
+
+            //hash table
+            Console.WriteLine("--- hashtable --------");
+            Hashtable ht = new Hashtable();
+            ht.Add("de","delhi");
+            ht.Add("hyd", "hyderabad");
+            ht.Add("che", "chennai");
+            ht.Remove("de");
+
+            foreach (DictionaryEntry k in ht)
+            {
+                Console.WriteLine(k.Key + " " + k.Value);
+            }
+
+            //sortedList
+            Console.WriteLine("--- sortedlist --------");
+            SortedList sl = new SortedList();
+            sl.Add(1,"Naresh");
+            sl.Add(2, "varun");
+            sl.Add(3, "Hari");
+
+            foreach (DictionaryEntry dic in sl)
+            {
+                Console.WriteLine(dic.Key + " " + dic.Value);
+              
+            }
+
+            //stack
+            Console.WriteLine("--- stack --------");
+            Stack stk = new Stack();
+            stk.Push("Hyderabad");
+            stk.Push("chennai");
+            stk.Push("bangalore");
+            stk.Push("mumbai");
+            stk.Pop();
+
+            foreach (object o in stk)
+            {
+                Console.WriteLine(o);
+            }
+
+            //queue
+            Console.WriteLine("--- Queue --------");
+            Queue q = new Queue();
+            q.Enqueue("cs.net");
+            q.Enqueue("vb.net");
+            q.Enqueue("asp.net");
+            q.Enqueue("sqlserver");
+            q.Dequeue();
+
+            foreach (object o in q)
+            {
+                Console.WriteLine(o);
+            }
+
+            Console.WriteLine("---------generic collections-----------");
+            Genericcollections gc = new Genericcollections();
+            gc.generics();
+        }
+    }
+}
