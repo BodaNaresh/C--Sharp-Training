d2aca54e36e9856cda4fae6ef0c0981ac1e3cb3d
BodaNaresh <100840377+BodaNaresh@users.noreply.github.com>
2022-09-09 06:29:50 -0700

    commit of day28 Task

---
+++ FileStreamProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using ReadandWriter;
+using System;
+using System.IO;
+
+namespace FileStreamProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Example e = new Example();
+            e.InsertEmployee();
+
+            FileStream f = new FileStream("d:\\Example.txt", FileMode.OpenOrCreate);
+
+            StreamWriter s = new StreamWriter(f);
+
+            Console.WriteLine("Writing data to file");
+            s.WriteLine(e.Name);
+            s.WriteLine(e.Mobile);
+            s.WriteLine(e.Salary);
+
+            s.Close();
+            f.Close();
+
+            e.reader();
+            e.TextWriter();
+            e.TextReader();
+
+           
+        }
+    }
+}
---
+++ ReadandWriter/Example.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ReadandWriter
+{
+    public class Example
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Mobile { get; set; }
+        public double Salary { get; set; }
+
+        SqlConnection con = new SqlConnection("server=localhost;database=Practice;Integrated Security=true; Encrypt=false");
+
+        public void InsertEmployee()
+        {
+            try
+            {
+                Console.WriteLine("Enter name of the employee ");
+                Name = Console.ReadLine();
+                Console.WriteLine("Enter mobile of the employee ");
+                Mobile = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter salary of the employee ");
+                Salary = double.Parse(Console.ReadLine());
+
+                SqlCommand cmd = new SqlCommand("insert into Emp values('" + Name + "','" +Mobile + "','" + Salary + "')", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Console.WriteLine("Data inserted sucessfully");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("enter correct data");
+            }
+            
+
+        }
+
+        public void reader()
+        {
+            FileStream f = new FileStream("d:\\Employee.txt", FileMode.OpenOrCreate);
+
+            StreamReader sr = new StreamReader(f);
+            Console.WriteLine("Reading data from the file");
+
+            string Na= sr.ReadLine();
+
+            Console.WriteLine("The data from the file is : " + Na);
+            
+            sr.Close();
+            f.Close();
+        }
+
+        public void TextWriter()
+        {
+            using (TextWriter writer = File.CreateText("d:\\Employee.txt"))
+            {
+
+                writer.WriteLine("The first line with text writer");
+            }
+            Console.ReadLine();
+        }
+        public void TextReader()
+        {
+            using (TextReader reader = File.OpenText("d:\\Employee.txt"))
+            {
+
+                String data = reader.ReadToEnd();
+                Console.WriteLine(data);
+            }
+            Console.ReadLine();
+            
+        }
+
+    }
+    
+}
